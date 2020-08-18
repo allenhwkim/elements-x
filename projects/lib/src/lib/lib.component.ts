4705f9b4b428703f6b9840b73e6132f26ecfcf4d
Allen Kim <allenhwkim@gmail.com>
2020-08-18 01:01:04 -0400

    first commit

---
+++ projects/lib/src/lib/lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib',
+  template: `
+    <p>
+      lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

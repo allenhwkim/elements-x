0ec30753b0a0dac6f551dad0f6567ebb8aee8b9b
Kim, Allen <allen.kim@ontariohealth.ca>
2025-01-09 17:55:51 -0500

    updated

--- lib/extended/formdesigner/plugins/bs-pad/index.ts
+++ lib/extended/formdesigner/plugins/bs-pad/index.ts
@@ -1,4 +1,4 @@
-import type { Editor, PluginOptions } from 'grapesjs';
+import type { Component, Editor, PluginOptions } from 'grapesjs';
 import css from './style';
 
 export default function(editor: Editor, options: PluginOptions) {
@@ -33,17 +33,21 @@ export default function(editor: Editor, options: PluginOptions) {
       editor.Canvas.toggleFramesEvents(false);
       // const pos = event.target.getAttribute('class').match(/\s(top|left|bottom|right)/)[1];
       const pos = event.target.getAttribute('class');
-      const pos1 = {top:'t', bottom:'b', left: 's', right: 'e'}[pos];
-      const oldKlass = component.getClasses().find(el => el.startsWith(`p${pos1}-`)) || `p${pos}-0`;
+      const posChar = {top:'t', bottom:'b', left: 's', right: 'e'}[pos];
+      const posXY = ['left', 'right'].includes(pos) ? 'x' : 'y';
+      const oldKlass1 = component.getClasses().find(el => el.startsWith('p-'));
+      const oldKlass2 = component.getClasses().find(el => el.startsWith(`p${posChar}-`));
+      const oldKlass3 = component.getClasses().find(el => el.startsWith(`p${posXY}-`));
+      const oldKlass = oldKlass3 || oldKlass2 || oldKlass1 || `p${pos}-0`;
       // const oldKlassVal = component.getAttribute(oldKlass);
       const oldPadVal = +oldKlass.split('-')[1];
 
       const newPadVal = (oldPadVal + 1) % 6;
-      const newKlass = `p${pos1}-${newPadVal}`;
+      const newKlass = `p${posChar}-${newPadVal}`;
 
-      // update px-x class with a new size, e.g. pt-5
-      component.removeClass(oldKlass)
-      component.addClass(newKlass)
+      // update p?-? class with a new size, e.g. pt-5
+      oldKlass.match(/p[a-z]-[0-9]/) && component.removeClass(oldKlass)
+      component.addClass(newKlass);
 
       // update #gjs-tools height so that the buttons located properly
       const compHeight = parseInt(window.getComputedStyle(component.getEl()).height, 10); 
